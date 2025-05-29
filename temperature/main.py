5d470a5f2e560afca59182fa6dfb4e8ef4f42ad4
Vocane <vocane@mailbox.org>
2025-05-29 21:42:58 +0300

    Add support for manually offsetting sensor values

--- temperature/main.py
+++ temperature/main.py
@@ -9,17 +9,17 @@ import wifi #type: ignore
 
 import adafruit_minimqtt.adafruit_minimqtt as MQTT
 
-wifi.radio.connect(os.getenv("CIRCUITPY_WIFI_SSID"), os.getenv("CIRCUITPY_WIFI_PASSWORD"))
-print(f"Connected to {os.getenv('CIRCUITPY_WIFI_SSID')}!")
+wifi.radio.connect(os.getenv('CIRCUITPY_WIFI_SSID'), os.getenv('CIRCUITPY_WIFI_PASSWORD'))
+print(f'Connected to {os.getenv("CIRCUITPY_WIFI_SSID")}')
 
 pool = socketpool.SocketPool(wifi.radio)
 ssl_context = ssl.create_default_context()
 
 mqtt_client = MQTT.MQTT(
-    broker=os.getenv("MQTT_BROKER", ""),
-    port=int(os.getenv("MQTT_PORT", 0)),
-    username=os.getenv("MQTT_USERNAME"),
-    password=os.getenv("MQTT_PASSWORD"),
+    broker=os.getenv('MQTT_BROKER', ''),
+    port=int(os.getenv('MQTT_PORT', 0)),
+    username=os.getenv('MQTT_USERNAME'),
+    password=os.getenv('MQTT_PASSWORD'),
     socket_pool=pool,
     ssl_context=ssl_context,
 )
@@ -33,8 +33,8 @@ while True:
         assert temp
         humidity = dht.humidity
         assert humidity
-        mqtt_client.publish('home/temperature', temp)
-        mqtt_client.publish('home/humidity', humidity)
+        mqtt_client.publish('home/temperature', temp + os.getenv('SENSOR_TEMP_OFFSET', 0.0))
+        mqtt_client.publish('home/humidity', humidity + os.getenv('SENSOR_HUMI_OFFSET', 0.0))
     except (RuntimeError, AssertionError):
         # Reading sensor failed, retry
         time.sleep(2)
